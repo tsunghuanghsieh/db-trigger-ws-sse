21ebd772324b03d2696f6bbca88093ddf62d8659
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2026-03-31 02:31:56 -0700

    Correct fix for conda init error message Instead of explicitly sourcing conda profile again, source this file to use current shell session. Remove conda env de/activation message at the end of conda create. Not rerun conda create and pip3 install when conda env already exists.

--- backend/setup.sh
+++ backend/setup.sh
@@ -2,22 +2,18 @@
 
 ENV_NAME=db-trigger-ws
 
-if [ -z "$CONDA_EXE" ]; then
-    # CONDA_EXE not set, exit with error
-    echo "Error: CONDA_EXE is not set" >&2
-    exit 1
-else
-    CONDA_BASE="$(dirname $(dirname "$CONDA_EXE"))"
+# source this file to set up the environment
+if [[ $- != *i* ]]; then
+    echo "Please run 'source $0' to set up the environment."
+    exit 0
 fi
 
-if [ -f "$CONDA_BASE/etc/profile.d/conda.sh" ]; then
-    source "$CONDA_BASE/etc/profile.d/conda.sh"
+env_exists=$(conda env list | grep -w $ENV_NAME)
+if [[ -z "$env_exists" ]]; then
+    conda create --name $ENV_NAME python=3.12 -y | sed '/^#$/,/^# To deactivate/ d'
+    conda activate $ENV_NAME
+    pip3 install -r "`dirname $0`/requirements.txt"
 else
-    echo "Error: conda.sh not found. Please install Conda." >&2
-    exit 1
+    conda activate $ENV_NAME
 fi
 
-# source this file
-conda create --name $ENV_NAME --clone base
-conda activate $ENV_NAME
-pip3 install -r "`dirname $0`/requirements.txt"
