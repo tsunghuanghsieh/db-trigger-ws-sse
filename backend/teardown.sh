c28e21aa446c9589fc90228275cd8ade252d6682
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2026-03-31 02:34:04 -0700

    Additional runtime checks Enforce sourcing this file to use current shell session. Remove conda env only when it exists.

--- backend/teardown.sh
+++ backend/teardown.sh
@@ -2,6 +2,14 @@
 
 ENV_NAME=db-trigger-ws-sse
 
-# source this file
+# source this file to tear down the environment
+if [[ $- != *i* ]]; then
+    echo "Please run 'source $0' to tear down the environment."
+    exit 0
+fi
+
 conda activate base
-conda env remove --name $ENV_NAME
+env_exists=$(conda env list | grep -w $ENV_NAME)
+if [[ -n "$env_exists" ]]; then
+    conda env remove --name $ENV_NAME -y
+fi
